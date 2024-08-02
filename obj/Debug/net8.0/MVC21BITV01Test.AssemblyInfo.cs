9a520c36a92cd800d9cc28641fd4549f8974e774
nguyentrungtinh <trungtinhn300@gmail.com>
2024-08-02 10:10:42 +0700

    Dung boostrap cap nhat giao dien view va create customer

--- obj/Debug/net8.0/MVC21BITV01Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVC21BITV01Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC21BITV01Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12b6a5e1cdce96bb8fe5fe0ea4f87f475e66ef28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b659fd49eede17056e6a46196797c0e2e8b4172")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC21BITV01Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC21BITV01Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
