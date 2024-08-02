a8c0850ea69c0b4e466957c8f01d5c7621b07923
VanTan22ab <izukumi5672@gmail.com>
2024-08-02 10:51:27 +0700

    Bổ sung file Khách hàng

--- Data/KhachHang.cs
+++ Data/KhachHang.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace MVC21BITV01Test.Data;
+namespace MVCMidterm.Data;
 
 public partial class KhachHang
 {
+    [Required(ErrorMessage = "Mã khách hàng không được để trống")]
     public string MaKh { get; set; } = null!;
-
+    [Required(ErrorMessage = "Tên công ty là bắt buộc")]
     public string TenCty { get; set; } = null!;
 
     public string? DiaChi { get; set; }
